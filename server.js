d516faf0eef8f509ab1e6cbf85fe614aea8e7410
sujal-shrestha <sujalwork69@gmail.com>
2025-12-22 00:45:06 +0545

    fix(server): remove duplicate venues routes and cleanup

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // server.js
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 dotenv.config();
 connectDB();
@@ -13,10 +13,11 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api/auth', require('./routes/auth'));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/venues", require("./routes/venues"));
 
-app.get('/', (req, res) => {
-  res.send('QueueLess API is running');
+app.get("/", (req, res) => {
+  res.send("QueueLess API is running");
 });
 
 const PORT = process.env.PORT || 5000;
