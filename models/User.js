9d6b49076d0634133f3e0c57e50dbfed27136be3
sujal-shrestha <sujalwork69@gmail.com>
2025-12-22 00:46:18 +0545

    chore(user): update User model password hashing and match method

--- models/User.js
+++ models/User.js
@@ -1,20 +1,26 @@
 // models/User.js
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true, unique: true },
-    email:    { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true, trim: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
     password: { type: String, required: true },
-    role:     { type: String, enum: ['user', 'staff'], default: 'user' },
+    role: { type: String, enum: ["user", "staff"], default: "user" },
   },
   { timestamps: true }
 );
 
 // hash password before save
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
@@ -25,4 +31,4 @@ userSchema.methods.matchPassword = function (enteredPassword) {
   return bcrypt.compare(enteredPassword, this.password);
 };
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
