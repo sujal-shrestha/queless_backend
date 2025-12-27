a10997b694625a1ee421ccff3c983d2681c87f16
sujal-shrestha <sujalwork69@gmail.com>
2025-12-27 23:33:14 +0545

    chore: register booking routes

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const bookingRoutes = require("./routes/bookingRoutes");
+
+
 
 dotenv.config();
 connectDB();
@@ -15,6 +18,7 @@ app.use(express.json());
 // Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/venues", require("./routes/venues"));
+app.use("/api/bookings", bookingRoutes);
 
 app.get("/", (req, res) => {
   res.send("QueueLess API is running");
